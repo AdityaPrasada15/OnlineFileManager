3861f728c5d5c3748de7301c6d844530358ceed6
Aditya Prasada <adityaprasada97@yahoo.com>
2025-07-28 00:32:05 +0530

    Project started.

---
+++ src/test/java/com/prasadaa/OnlineFileManager/OnlineFileManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prasadaa.OnlineFileManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineFileManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
